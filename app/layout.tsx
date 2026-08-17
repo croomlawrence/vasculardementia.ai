6a6357c9a87f8faabd911efa2cc0ac4ecfd2fc16
Croom Lawrence <croomlawrence@mac.mynetworksettings.com>
2026-08-17 11:56:14 -0400

    feat: install Google Tag Manager

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import { Analytics } from "@vercel/analytics/react";
 import Disclaimer from "./components/Disclaimer";
 import MarketingPixels from "./components/MarketingPixels";
 import NavDropdown from "./components/NavDropdown";
+import Script from "next/script";
 
 const geistSans = Geist({ variable: "--font-geist-sans", subsets: ["latin"] });
 const geistMono = Geist_Mono({ variable: "--font-geist-mono", subsets: ["latin"] });
@@ -66,7 +67,18 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
 
   return (
     <html lang="en" className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}>
+      <Script id="google-tag-manager">
+        {`
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-PTS44QT6');
+        `}
+      </Script>
       <body className="min-h-full flex flex-col">
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PTS44QT6"
+        height="0" width="0" style={{display:"none",visibility:"hidden"}}></iframe></noscript>
         <MarketingPixels />
         <script type="application/ld+json" dangerouslySetInnerHTML={{ __html: JSON.stringify(orgSchema) }} />
         <nav className="site-nav border-b bg-white sticky top-0 z-40">
